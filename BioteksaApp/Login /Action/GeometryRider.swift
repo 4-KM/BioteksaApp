1a60321525f24e0e7bbb42bfb48d0a4e01cdd1ca
gersai <gersai_ventura@hotmail.com>
2023-11-08 00:42:57 -0600

    se conecto copn los servicios de el postmand se modifco el login  se modifico la primera tabla de acidos

---
+++ BioteksaApp/Login /Action/GeometryRider.swift
@@ -0,0 +1,8 @@
+//
+//  GeometryRider.swift
+//  BioteksaApp
+//
+//  Created by Eduardo Gersai Garcia Ventura on 14/10/23.
+//
+
+import Foundation
---
+++ Package.swift
@@ -0,0 +1,8 @@
+//
+//  Package.swift
+//  BioteksaApp
+//
+//  Created by Eduardo Gersai Garcia Ventura on 10/10/23.
+//
+
+import Foundation
